5ec6f6676a44f57708164158259c2987a3706d18
Quaaas <sebastian.gesslein@gmx.net>
2026-06-17 08:13:24 -0400

    added emotion_detiction module

---
+++ emotion_detection.py
@@ -0,0 +1,33 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = myobj, headers=header)
+
+
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+    anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+    disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+    fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+    joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+    sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+
+    output = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+    }
+    output["dominant_emotion"] = max(output, key=output.get)
+    # Return the label and score in a dictionary
+    return output
+
+
+if __name__ == "__main__":
+    output = emotion_detector("I love this new technology.")
+    print(output)
